ba8b15fa32c5455564086a1b9308c3771fbba4e5
amalfranci <amalfrancis744@gmail.com>
2025-03-11 10:44:59 +0530

    shadn button added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function About() {
+    return (
+        <>
+            <div className="font-semibold flex flex-col items-center  justify-center m-3 border-2 bg-amber-300 p-2">
+                <h1>Hello Guyss</h1>
+                <div><Button>Hello</Button></div>
+            </div>
+        </>
+    );
+}
